0bb517a3e801368cb8acd3d8a86bea34e09bf3ab
Fernando Baeza Hurtado <91624660+ferbaeza@users.noreply.github.com>
2021-12-01 23:17:17 +0100

    main branch

--- babelExpress/src/index.js
+++ babelExpress/src/index.js
@@ -3,4 +3,14 @@ import ReactDOM from 'react-dom';
 const Index = () => {
     return <div>WELCOME TO REACT APP!</div>;
 };
+const Login=()=>{
+    return(
+        <form>
+            <input type="text" placeholder="Put your name"></input>
+        </form>
+    )
+}
+
+
 ReactDOM.render(<Index />,document.getElementById('app'));
+ReactDOM.render(<Login />,document.getElementById('login'));
